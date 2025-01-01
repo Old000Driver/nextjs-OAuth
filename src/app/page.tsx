2e7af0353260ea2072a03c5f34561e37399c5bd5
GJA <792002710@qq.com>
2025-01-01 12:52:04 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "@/components/sign-in";
+import UserAvatar from "@/components/UserAvatar";
+export default function Home() {
+  return (
+    <div>
+      <SignIn />
+      <UserAvatar />
+    </div>
+  );
+}
